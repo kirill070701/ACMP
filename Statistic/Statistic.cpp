1324f912c2151d144546fcd2a25c41b0359c1439
Kirill <kirill07.07.01@mail.ru>
2024-09-15 20:35:46 +0300

    ACMP

--- Statistic/Statistic.cpp
+++ Statistic/Statistic.cpp
@@ -1,26 +1,85 @@
 ﻿#include <iostream>
+#include <cstdlib>
+#include <string>
 
 using namespace std;
 
-void String_to_Int(string*, int);
+struct Even_numbers {
+    size_t number;
+    struct Even_numbers* next;
+};
+
+struct Non_even_numbers {
+    size_t number;
+    struct Non_even_numbers* next;
+};
+
 
 int main()
 {
-    int N_quanty_day;
-    string N_day;
+    size_t quantity_even_day            = 0;
+    size_t quantity_non_even_day        = 0;
+    size_t quantity_day;
+    cin >> quantity_day;
 
-    cin >> N_quanty_day;
-    cin >> N_day;
+    Even_numbers* N_even_numbers                        = new Even_numbers;
+    Non_even_numbers* N_non_even_numbers                = new Non_even_numbers;
 
-    int data[2][2];
+    Even_numbers* link_N_even_numbers                   = N_even_numbers;
+    Non_even_numbers* link_N_non_even_numbers           = N_non_even_numbers;
 
-    String_to_Int(&N_day, N_quanty_day);
+    Even_numbers* start_regist_even_numbers             = N_even_numbers;
+    Non_even_numbers* start_regist_non_even_numbers     = N_non_even_numbers;
 
-}
-
-void String_to_Int(string* N_day, int N_quanty_day) {
-    for (size_t i = 0; i < N_quanty_day; i++)
+    size_t number_cin;
+    
+    for (size_t i = 0; i < quantity_day; i++)
     {
-        cout << (*N_day)[i];
+        cin >> number_cin;
+
+        if (number_cin % 2 == 0)
+        {
+            N_even_numbers->number          = number_cin;
+            N_even_numbers                  = new Even_numbers;
+            link_N_even_numbers->next       = N_even_numbers;
+            link_N_even_numbers             = N_even_numbers;
+
+            quantity_even_day++;
+        }
+        else {
+            N_non_even_numbers->number      = number_cin;
+            N_non_even_numbers              = new Non_even_numbers;
+            link_N_non_even_numbers->next   = N_non_even_numbers;
+            link_N_non_even_numbers         = N_non_even_numbers;
+
+            quantity_non_even_day++;
+        }
     }
+
+    N_non_even_numbers = start_regist_non_even_numbers;
+    N_even_numbers = start_regist_even_numbers;
+
+    for (size_t i = 0; i < quantity_non_even_day; i++)
+    {
+        cout << N_non_even_numbers->number;
+        N_non_even_numbers = N_non_even_numbers->next;
+        if (i + 1 != quantity_non_even_day) {
+            cout << " ";
+        }
+    }
+
+    cout << "\n";
+
+    for (size_t i = 0; i < quantity_even_day; i++)
+    {
+        cout << N_even_numbers->number;
+        N_even_numbers = N_even_numbers->next;
+        if (i + 1 != quantity_even_day) {
+            cout << " ";
+        }
+    }
+
+    cout << "\n";
+
+    quantity_even_day >= quantity_non_even_day ? cout << "YES" : cout << "NO";
 }
